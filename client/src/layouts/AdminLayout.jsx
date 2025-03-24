1887eea813964e10a231e24aeed219b321d22c6d
Johan Johansson <speloch@gmail.com>
2025-03-24 11:25:42 +0100

    Added Layout folders

---
+++ client/src/layouts/AdminLayout.jsx
@@ -0,0 +1,16 @@
+
+
+const AdminLayout = () => {
+    return (
+      <>
+        <header>
+          <h1>Administration</h1>
+        </header>
+        <main>
+          
+        </main>
+      </>
+    );
+  };
+  
+  export default AdminLayout;
---
+++ client/src/layouts/DefaultLayout.jsx
@@ -0,0 +1,11 @@
+
+
+const DefaultLayout = () => {
+    return (
+      <>
+        
+      </>
+    );
+  };
+  
+  export default DefaultLayout;
