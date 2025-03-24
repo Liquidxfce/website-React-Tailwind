f16778202f13e304815f7c9cc48d3d8247de9bb6
Johan Johansson <speloch@gmail.com>
2025-03-24 15:35:34 +0100

    Update links and Layouts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Route, Routes } from 'react-router'
 import TopBar from "./components/topBar/topBar";
 import BottomInfo from "./components/bottomInfo/bottomInfo";
 import ProductGrid from "./components/ProductGrid/ProductGrid";
@@ -18,6 +19,9 @@ function App() {
       <footer>
         <BottomInfo />
       </footer>
+      <Routes>
+        <Route></Route>
+      </Routes>
     </>
   )
 }
--- client/src/layouts/DefaultLayout.jsx
+++ client/src/layouts/DefaultLayout.jsx
@@ -1,9 +1,20 @@
-
+import { Link, Outlet } from "react-router";
+import BottomInfo from "../components/bottomInfo/bottomInfo";
 
 const DefaultLayout = () => {
     return (
       <>
-        
+        <header>
+            <Link to="/">
+            
+            </Link>
+        </header>
+        <main>
+            <Outlet />
+        </main>
+        <footer>
+            <BottomInfo />
+        </footer>
       </>
     );
   };
