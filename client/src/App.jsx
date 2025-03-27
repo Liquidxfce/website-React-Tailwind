ad853063b736bf5f0def1beb9fbb751dbdff4f39
Johan Johansson <speloch@gmail.com>
2025-03-27 11:28:00 +0100

    Removed import of TopBar, productGrid and footerInfo from app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,5 @@
 import "./App.css";
 import { Route, Routes } from 'react-router'
-import TopBar from "./components/topBar/topBar";
-import BottomInfo from "./components/bottomInfo/bottomInfo";
-import ProductGrid from "./components/ProductGrid/ProductGrid";
 import AdminLayout from "./layouts/AdminLayout"
 import DefaultLayout from "./layouts/DefaultLayout";
 
@@ -10,17 +7,6 @@ function App() {
   
   return (
     <>
-      <header>
-        <TopBar />
-      </header>
-      <main>
-        <section>
-          <ProductGrid/>
-        </section>
-      </main>
-      <footer>
-        <BottomInfo />
-      </footer>
       <Routes>
         <Route element={<DefaultLayout />}>
           <Route></Route>
