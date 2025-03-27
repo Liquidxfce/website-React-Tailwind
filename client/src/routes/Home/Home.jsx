f5dc0c00e6a0d5b035ccb3564299d590bee341ca
Johan Johansson <speloch@gmail.com>
2025-03-27 13:17:35 +0100

    Added ProductGrid to Home

--- client/src/routes/Home/Home.jsx
+++ client/src/routes/Home/Home.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
+import ProductGrid from "../../components/ProductGrid/ProductGrid";
 // TODO: uppdate to work with my project
 
 const Home = () => {
  
   return (
     <>
-     
+      <ProductGrid />
     </>
   );
 };
