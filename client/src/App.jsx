ff4f1296e7ee1f0fed41ff377f8a4ee55cdb5557
Johan Johansson <speloch@gmail.com>
2025-04-09 09:58:21 +0200

    Update routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import DefaultLayout from "./layouts/DefaultLayout";
 import Home from "./routes/Home/Home";
 import ProductDetails from "./routes/ProductDetails/ProductDetails";
 import NewProduct from "./routes/NewProduct/NewProduct";
+import AdminPage from "./routes/AdminPage/AdminPage";
 
 function App() {
   
@@ -15,8 +16,9 @@ function App() {
           <Route index element={<Home />}/>
           <Route path="/products/:id" element={<ProductDetails />} />
         </Route>
-        <Route path="/admin" element={<AdminLayout />}>
-          <Route path="products/new" element={<NewProduct />} />
+        <Route path="/admin/" element={<AdminLayout />}>
+          <Route path="/admin/products/" element={<AdminPage />} />
+          <Route path="/admin/products/new" element={<NewProduct />} />
         </Route>
       </Routes>
     </>
